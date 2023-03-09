972f5aca96b7c377925c93b4759a7e1ee9a48f14
minh <minhtrantbh@gmail.com>
2023-03-09 09:42:29 +0700

    first commit

---
+++ tranvanminh/Webbanhang/Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webbanhang.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult User()
+        {
+            return View();
+        }
+    }
+}
